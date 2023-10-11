63855827230608993e43e116d7ea5555269242c7
Oliver L <oliver.lkm@gmail.com>
2023-10-11 15:56:18 +1100

    Enable creation of new tasks under each project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import { renderBreadcrumbNav } from "./nav";
 import { projects, Project, renderProjectCard } from "./project";
-import { renderTaskCard } from "./task";
+import { Task, renderTaskCard } from "./task";
 import "./style.css";
 
 let view = "projects";
+let currentProject;
 const itemContainer = document.querySelector(".item-container");
-const formContainer = document.querySelector(".form-container");
-const inputProjectTitle = document.querySelector("#project-title");
+const projectFormContainer = document.querySelector(".project-form-container");
+const taskFormContainer = document.querySelector(".task-form-container");
+const inputProjectTitle = document.querySelector("#input-project-title");
+const inputTaskTitle = document.querySelector("#input-task-title");
+const inputTaskDescription = document.querySelector("#input-task-description");
+const inputTaskDueDate = document.querySelector("#input-task-due-date");
 const btnNewProject = document.querySelector(".btn-new-project");
+
+const unloadProjectForm = function () {
+  projectFormContainer.classList.add("hidden");
+};
+
+const unloadTaskForm = function () {
+  taskFormContainer.classList.add("hidden");
+};
 
 const clearItemContainer = function () {
   itemContainer.innerHTML = "";
@@ -16,6 +29,7 @@ const clearItemContainer = function () {
 const loadProjectsView = function () {
   view = "projects";
   renderBreadcrumbNav("projectsView");
+  unloadTaskForm();
   clearItemContainer();
   renderAllProjects(projects);
   enableTasksViewFromProjectCards();
@@ -30,6 +44,7 @@ const renderAllProjects = function (projects) {
 const loadTasksView = function (currentProject) {
   view = "tasks";
   renderBreadcrumbNav("tasksView", currentProject);
+  unloadProjectForm();
   clearItemContainer();
   renderTasks(currentProject.tasks);
   enableViewAllProjectsBtn();
@@ -46,8 +61,11 @@ const enableTasksViewFromProjectCards = function () {
   projectCardsNL.forEach((projectCard) => {
     const clickedProjectId = projectCard.children[0].getAttribute("id");
     for (let [key, value] of Object.entries(projects)) {
-      if (value.id + `${value.title}` === clickedProjectId) {
-        projectCard.addEventListener("click", () => loadTasksView(value));
+      if (value.id + value.title === clickedProjectId) {
+        projectCard.addEventListener("click", () => {
+          loadTasksView(value);
+          currentProject = value;
+        });
         break;
       }
     }
@@ -64,24 +82,52 @@ const enableViewAllProjectsBtn = function () {
 loadProjectsView();
 
 const loadProjectForm = function () {
-  formContainer.classList.remove("hidden");
+  projectFormContainer.classList.remove("hidden");
   inputProjectTitle.focus();
   const btnSaveProject = document.querySelector(".btn-save-project");
   btnSaveProject.addEventListener("click", saveProject);
 };
 
 const saveProject = function () {
-  let projectId = new Date().getTime();
-  projects[projectId] = new Project(inputProjectTitle.value);
+  let projectId = `P${new Date().getTime()}`;
+  projects[projectId] = new Project(inputProjectTitle.value || "New Project");
   inputProjectTitle.value = "";
-  formContainer.classList.add("hidden");
+  projectFormContainer.classList.add("hidden");
   console.log(projects);
   console.log(projects[projectId]);
   clearItemContainer();
   loadProjectsView();
 };
 
+const loadTaskForm = function () {
+  taskFormContainer.classList.remove("hidden");
+  inputTaskTitle.focus();
+  const btnSaveTask = document.querySelector(".btn-save-task");
+  btnSaveTask.addEventListener("click", saveTask);
+};
+
+const saveTask = function () {
+  console.log(currentProject);
+  console.log(projects[currentProject]);
+  currentProject.tasks.push(
+    new Task(
+      inputTaskTitle.value || "New Task",
+      inputTaskDescription.value || ". . .",
+      inputTaskDueDate.value || "( no due date )",
+      "pending",
+      "medium"
+    )
+  );
+  inputTaskTitle.value = "";
+  inputTaskDescription.value = "";
+  taskFormContainer.classList.add("hidden");
+  console.log(projects);
+  console.log(currentProject.tasks);
+  clearItemContainer();
+  loadTasksView(currentProject);
+};
+
 btnNewProject.addEventListener("click", () => {
   if (view === "projects") loadProjectForm();
-  // if (view === "tasks") loadTaskForm();
+  if (view === "tasks") loadTaskForm();
 });
