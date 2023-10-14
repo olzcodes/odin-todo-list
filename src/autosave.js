c3650f5e8da0cdaafb29b83fea82e932f0157b16
Oliver L <oliver.lkm@gmail.com>
2023-10-15 00:17:34 +1100

    Initial commit

---
+++ src/autosave.js
@@ -0,0 +1,62 @@
+import { projects } from "./project";
+
+const autoSaveProjectTitleChanges = function (projectId, title) {
+  projects[projectId].title = title.value;
+};
+
+export const inputHandlerProjectTitle = function () {
+  const projectTitlesNL = document.querySelectorAll(".input-project-title");
+
+  if (!projectTitlesNL) return;
+
+  projectTitlesNL.forEach((title) =>
+    title.addEventListener("input", (e) => {
+      const projectId = e.target.closest(".project-card").dataset.projectId;
+      autoSaveProjectTitleChanges(projectId, title);
+    })
+  );
+};
+
+const autoSaveTaskTitleChanges = function (targetProject, taskId, taskTitle) {
+  targetProject.tasks.forEach((task) => {
+    if (task.id === taskId) task.title = taskTitle.value;
+  });
+};
+
+export const inputHandlerTaskTitle = function (targetProject) {
+  const taskTitlesNL = document.querySelectorAll(".input-task-title");
+
+  if (!taskTitlesNL) return;
+
+  taskTitlesNL.forEach((title) =>
+    title.addEventListener("input", (e) => {
+      const taskId = e.target.closest(".task-card").dataset.taskId;
+      autoSaveTaskTitleChanges(targetProject, taskId, title);
+    })
+  );
+};
+
+const autoSaveTaskDescriptionChanges = function (
+  targetProject,
+  taskId,
+  taskDescription
+) {
+  targetProject.tasks.forEach((task) => {
+    if (task.id === taskId) task.description = taskDescription.value;
+  });
+};
+
+export const inputHandlerTaskDescription = function (targetProject) {
+  const taskDescriptionsNL = document.querySelectorAll(
+    ".input-task-description"
+  );
+
+  if (!taskDescriptionsNL) return;
+
+  taskDescriptionsNL.forEach((description) =>
+    description.addEventListener("input", (e) => {
+      const taskId = e.target.closest(".task-card").dataset.taskId;
+      autoSaveTaskDescriptionChanges(targetProject, taskId, description);
+    })
+  );
+};
