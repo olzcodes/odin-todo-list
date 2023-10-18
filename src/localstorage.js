a5c9549ddfd18a5ae6a1f5215d4940b0bcbe9393
Oliver L <oliver.lkm@gmail.com>
2023-10-19 00:26:16 +1100

    Make loadFromLocalStorage function re-create project objects including methods

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,10 +1,18 @@
 import { projects } from "./index";
+import { Project } from "./project";
+import { demoProjects } from "./demoData";
 
 export const saveToLocalStorage = function () {
   localStorage.setItem("projects", JSON.stringify(projects));
 };
 
 export const loadFromLocalStorage = function () {
-  let storedProjectsJSON = JSON.parse(localStorage.getItem("projects"));
-  return storedProjectsJSON;
+  const storedProjectsJSON =
+    JSON.parse(localStorage.getItem("projects")) || demoProjects;
+
+  let projectObjects = {};
+  for (const [key, value] of Object.entries(storedProjectsJSON)) {
+    projectObjects[key] = new Project(value.id, value.title, value.tasks);
+  }
+  return projectObjects;
 };
