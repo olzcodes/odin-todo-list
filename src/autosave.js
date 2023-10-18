928f63a1a11f30dfb02fef7a47257d6ff5cff6ac
Oliver L <oliver.lkm@gmail.com>
2023-10-19 00:32:51 +1100

    Update import source

--- src/autosave.js
+++ src/autosave.js
@@ -1,4 +1,4 @@
-import { projects } from "./index.js";
+import { projects } from "./index";
 import { saveToLocalStorage } from "./localStorage";
 
 const autoSaveProjectTitleChanges = function (projectId, title) {
