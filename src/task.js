65951713fefbf29a562a8cbb3985131eb1b19519
Oliver L <oliver.lkm@gmail.com>
2023-10-19 21:38:19 +1100

    Modify Task class to include inputStatus property

--- src/task.js
+++ src/task.js
@@ -1,12 +1,12 @@
 export class Task {
-  constructor(title, description, dueDate, status, priority) {
+  constructor(title, description, dueDate, status, priority, inputStatus) {
     this.id = `T${new Date().getTime()}`;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.status = status;
     this.priority = priority;
-    this.inputStatus = "enabled";
+    this.inputStatus = inputStatus;
   }
 }
 
