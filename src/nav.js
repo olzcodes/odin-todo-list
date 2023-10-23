a198569d823c0ed9520b55c822b9e14cd1cb5514
Oliver L <oliver.lkm@gmail.com>
2023-10-24 00:53:08 +1100

    Add ability to create multiple instances of breadcrumb navigation

--- src/nav.js
+++ src/nav.js
@@ -1,20 +1,26 @@
-const breadcrumbNav = document.querySelector(".breadcrumb-nav");
+const breadcrumbNav = document.querySelectorAll(".breadcrumb-nav");
 
 export const renderBreadcrumbNav = function (mode, currentProject) {
   if (mode === "projectsView") {
-    breadcrumbNav.innerHTML = ``;
-    breadcrumbNav.innerHTML = `
-  <span class="accent">All Projects</span>
+    breadcrumbNav.forEach((element) => (element.innerHTML = ``));
+    breadcrumbNav.forEach(
+      (element) =>
+        (element.innerHTML = `
+  <span class="all-projects">All Projects</span>
   <span class="separator">></span>
-  `;
+  `)
+    );
   }
 
   if (mode === "tasksView") {
-    breadcrumbNav.innerHTML = ``;
-    breadcrumbNav.innerHTML = `
+    breadcrumbNav.forEach((element) => (element.innerHTML = ``));
+    breadcrumbNav.forEach(
+      (element) =>
+        (element.innerHTML = `
   <button class="btn-view-all-projects">All Projects</button>
   <span class="separator">></span>
-  <span>${currentProject.title}</span>
-  `;
+  <span class="current-project">${currentProject.title}</span>
+  `)
+    );
   }
 };
