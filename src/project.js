21dadd1dbe567a7564477f3b5756ef24d02d8d15
Oliver L <oliver.lkm@gmail.com>
2023-10-26 23:16:35 +1100

    Move rendering function to View module

--- src/project.js
+++ src/project.js
@@ -14,38 +14,3 @@ export class Project {
     this.tasks = remainingTasks;
   }
 }
-
-export const renderProjectCard = function (project) {
-  const renderProjectDetails = function (project) {
-    let taskCount = 0;
-    project.tasks.forEach((task) => {
-      if (task.status === "pending") taskCount += 1;
-    });
-    return `${taskCount} pending tasks`;
-  };
-
-  const projectCard = document.createElement("div");
-  projectCard.className = "project-card";
-  projectCard.dataset.projectId = project.id;
-  projectCard.innerHTML = `
-  <div class="project-header">
-    <div class="project-header-left">
-      <input class="input-project-title" value="${
-        project.title
-      }" maxlength="28"/>
-    </div>
-    <div class="project-header-right">
-      <button class="btn-project-move-down">ᐯ</button>
-      <button class="btn-project-move-up">ᐱ</button>
-      <button class="btn-project-delete">X</button>
-    </div>
-  </div>
-  <div class="project-details">
-    <div>
-      ${renderProjectDetails(project)}
-    </div>
-  </div>
-  `;
-
-  return projectCard;
-};
