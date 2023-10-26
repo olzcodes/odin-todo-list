4e1f1d6e1ae6b464536e368d615f771f9516cb13
Oliver L <oliver.lkm@gmail.com>
2023-10-26 23:16:19 +1100

    Move rendering function to View module

--- src/task.js
+++ src/task.js
@@ -9,32 +9,3 @@ export class Task {
     this.inputStatus = inputStatus;
   }
 }
-
-export const renderTaskCard = function (task) {
-  const taskCard = document.createElement("div");
-  taskCard.className = "task-card";
-  taskCard.dataset.taskId = task.id;
-  taskCard.classList.add(`${task.status}`);
-  taskCard.innerHTML = `
-    <div class="task-header ${task.status}">
-      <div class="task-header-left">
-        <button class="btn-task-status ${task.status}"></button>
-        <input class="input-task-title ${task.status}" value="${task.title}"  ${task.inputStatus}/>
-      </div>
-      <div class="task-header-right">
-        <button class="btn-task-move-down">ᐯ</button>
-        <button class="btn-task-move-up">ᐱ</button>
-        <button class="btn-task-delete">X</button>
-      </div>
-    </div>
-    <div class="task-details ${task.status}">
-      <textarea class="input-task-description ${task.status}" name="task-description" ${task.inputStatus}>${task.description}</textarea>
-      <div class="task-bottom-row">
-        <input type="date" class="input-task-due-date" value="${task.dueDate}" ${task.inputStatus}/>
-        <button class="btn-task-options">...</button>
-      </div>
-    </div>
-  `;
-
-  return taskCard;
-};
