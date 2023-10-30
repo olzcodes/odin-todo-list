e0d1928cfe26e119157df4217d2a2f7320424d4e
Oliver L <oliver.lkm@gmail.com>
2023-10-30 12:42:55 +1100

    Add new method to Task class

--- src/task.js
+++ src/task.js
@@ -1,11 +1,28 @@
+import { differenceInCalendarDays } from "date-fns";
+
 export class Task {
-  constructor(title, description, dueDate, status, priority, inputStatus) {
-    this.id = `T${new Date().getTime()}`;
+  constructor(id, title, description, dueDate, status, priority, inputStatus) {
+    this.id = id;
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.status = status;
     this.priority = priority;
     this.inputStatus = inputStatus;
+  }
+
+  getDaysRemaining() {
+    const today = new Date();
+    const dueDate = Date.parse(this.dueDate);
+
+    if (!this.dueDate) return `no due date`;
+
+    const daysRemaining = differenceInCalendarDays(dueDate, today);
+
+    if (daysRemaining < -1) return `due ${-daysRemaining} days ago`;
+    if (daysRemaining === -1) return `due 1 day ago`;
+    if (daysRemaining === 0) return `due today`;
+    if (daysRemaining === 1) return `due in 1 day`;
+    if (daysRemaining > 1) return `due in ${daysRemaining} days`;
   }
 }
